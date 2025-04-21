aff10d1398a9209e3878ed1a4aa78b74d0d858f8
Daniel Guo <dguo83212@gmail.com>
2025-04-21 11:37:24 -0500

    Checking

--- Ducks TowerDefense Game/Assets/Code/GameLogic/Enemy.cs
+++ Ducks TowerDefense Game/Assets/Code/GameLogic/Enemy.cs
@@ -3,6 +3,7 @@ using UnityEngine.UI;
 using System.Collections;
 using UnityEngine.AI;
 
+// This script is responsible for enemy behavior, including movement, health management, and special abilities.
 public class Enemy : MonoBehaviour{
 
     private Enemy enemy; // Reference to the Enemy component
