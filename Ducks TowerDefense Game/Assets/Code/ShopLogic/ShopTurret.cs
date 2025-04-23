85b2f5a57a1264bdca5e1983d93ebdbbcf6eae1d
Daniel Guo <dguo83212@gmail.com>
2025-04-23 17:56:20 -0500

    Add Lockable and small bug fix
    Lockable

--- Ducks TowerDefense Game/Assets/Code/ShopLogic/ShopTurret.cs
+++ Ducks TowerDefense Game/Assets/Code/ShopLogic/ShopTurret.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Shop : MonoBehaviour{
 
+    [Header("Turret References")]
     public TurretBlueprint BlobTurret;
     public TurretBlueprint DuckTurret;
     public TurretBlueprint LaserBeamerTurret;
@@ -9,9 +11,26 @@ public class Shop : MonoBehaviour{
     public TurretBlueprint GatlingTurret;
     public TurretBlueprint FreezeTurret;
     public TurretBlueprint NukeTurret;
-    PlaceTurret placeTurret;
+
+    [Header("UI Buttons")]
+    public Button gatlingButton;
+    public Button freezeButton;
+    public Button nukeButton;
+
+    private PlaceTurret placeTurret;
+
     void Start(){
         placeTurret = PlaceTurret.instance;
+
+        // Determine if turrets are unlocked based on PlayerPrefs
+        GatlingTurret.isUnlocked = PlayerPrefs.GetInt("GatlingUnlocked", 0) == 1;
+        FreezeTurret.isUnlocked = PlayerPrefs.GetInt("FreezeUnlocked", 0) == 1;
+        NukeTurret.isUnlocked = PlayerPrefs.GetInt("NukeUnlocked", 0) == 1;
+
+        // Update button states based on unlocked status
+        if (gatlingButton) gatlingButton.interactable = GatlingTurret.isUnlocked;
+        if (freezeButton) freezeButton.interactable = FreezeTurret.isUnlocked;
+        if (nukeButton) nukeButton.interactable = NukeTurret.isUnlocked;
     }
     public void purchaseBlobTurret(){
         Debug.Log("Turret Selected");
