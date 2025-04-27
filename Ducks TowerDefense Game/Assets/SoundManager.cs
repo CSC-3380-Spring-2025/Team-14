e99666c86bc72a082367e515f505d2fdbadf52fb
jakypier <jpiers5@lsu.edu>
2025-04-26 23:40:38 -0500

    Mute Button in Options on MainMenu Scene
    Added Mute Button

---
+++ Ducks TowerDefense Game/Assets/SoundManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    private bool isMuted = false;
+
+    public void ToggleMute()
+    {
+        isMuted = !isMuted;
+        if (isMuted)
+        {
+            audioMixer.SetFloat("Volume", -80f); // basically silent
+        }
+        else
+        {
+            audioMixer.SetFloat("Volume", 0f); // normal volume
+        }
+    }
+}
